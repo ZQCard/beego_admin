5f597bad50da6a3cc24194f093c7a7b4c4362798
ZQCard <445864742@qq.com>
2019-07-03 23:56:34 +0800

    excel页面完善

--- controllers/admin/tools.go
+++ controllers/admin/tools.go
@@ -90,6 +90,7 @@ func (c *ToolsController)PostSendEmail()  {
 /***************** excel导入导出功能结束 ******************/
 func (c *ToolsController)GetExcel()  {
 	c.Data["Title"] = "excel导入导出功能"
+	c.Data["ExampleExcelUrl"] = beego.AppConfig.String("base_url") + "static/example/excelDemo.xlsx"
 	// 模板
 	c.TplName = "admin/tools/excel.html"
 }
@@ -103,31 +104,12 @@ func (c *ToolsController)PostExcelImport()  {
 	}
 	// 关闭句柄
 	defer file.Close()
-	// 根据日期保存文件目录
-	today := time.Now().Format("2006-01-02")
-	directory := "static/uploadFile/" + today + "/"
-	// 建立文件夹
-	err = os.MkdirAll(directory, os.ModePerm)
-	if err != nil {
-		log.Fatal("建立文件夹失败：", err)
-	}
-
-	filePath := directory + utils.RandString(10)+"_"+handler.Filename
-	// 混淆文件名前缀，并保存文件到指定地点
-	err = c.SaveToFile("file", filePath)
-	if err != nil {
-		log.Fatal("接收文件失败：", err)
-	}
+	fmt.Println(file)
+	fmt.Println(handler.Filename)
 
 	returnJson:= ResponseJson{}
-	if err != nil {
-		returnJson.StatusCode = Fail
-		returnJson.Message = "文件上传失败"
-	}
 
 	returnJson.StatusCode = Success
-	fmt.Println(beego.AppConfig.String("base_url"))
-	returnJson.Url = beego.AppConfig.String("base_url") + filePath
 	c.Data["json"] = &returnJson
 	c.ServeJSON()
 }
